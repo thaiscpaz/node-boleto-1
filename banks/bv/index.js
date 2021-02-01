23b0885ed69190a7cae58d12a8627ed0df4e3e34
Veller <vellerbauer@gmail.com>
2021-02-01 16:32:10 -0300

    changing bv logo img size

--- banks/bv/index.js
+++ banks/bv/index.js
@@ -4,7 +4,7 @@ var ediHelper = require('../../lib/edi-helper')
 var helper = require('./helper')
 
 exports.options = {
-  logoURL: '../../public/images/bv.jpg',
+  logoURL: '../../public/images/bvcopy.jpg',
   codigo: '655'
 }
 
